29be91909d261389c61893791edcda7c2fa024c1
Mickael de Raëmy <mickaelderaemy@gmail.com>
2016-03-12 12:54:06 +0100

    Files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/* Lightbox - Fancybox */
+$(document).ready(function() {
+
+	$(".fancybox").fancybox();
+
+
+	$( ".cross" ).hide();
+	$( ".menu" ).hide();
+	$( ".hamburger" ).click(function() {
+	$( ".menu" ).slideToggle( "slow", function() {
+	$( ".hamburger" ).hide();
+	$( ".cross" ).show();
+	});
+	});
+
+	$( ".cross" ).click(function() {
+	$( ".menu" ).slideToggle( "slow", function() {
+	$( ".cross" ).hide();
+	$( ".hamburger" ).show();
+	});
+	});
+
+});
